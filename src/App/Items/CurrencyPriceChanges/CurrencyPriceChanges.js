85f9a97ea8096915152fa911244fa4cfb9590339
TECHNIKAL_mohammad <mb.king1380@gmail.com>
2021-05-26 16:36:05 +0430

    add CurrencyPriceChanges api in report page

--- src/App/Items/CurrencyPriceChanges/CurrencyPriceChanges.js
+++ src/App/Items/CurrencyPriceChanges/CurrencyPriceChanges.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CurrencyPriceChanges extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            Currencys: []
+
+        }
     }
-    render() { 
-        return ( <p>CurrencyPriceChanges</p> );
+
+    componentDidMount() {
+        axios.get(`https://api.accessban.com/v1/data/sana/json`)
+            .then(res => {
+                const Currencys = res.data.sana.data;
+                this.setState({ Currencys });
+            }
+        )
+    }
+    render() {
+        setInterval(() => {
+            console.log("hi")
+        }, 60000);
+        return (
+            <ul>
+                { this.state.Currencys.map(Currency => <li>{Currency.title}{Currency.p}</li>)}
+            </ul>
+        )
     }
 }
- 
 export default CurrencyPriceChanges;
