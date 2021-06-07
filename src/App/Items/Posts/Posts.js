a2dda1d6afd81af783f83e4354c652616c8e9714
TECHNIKAL_mohammad <mb.king1380@gmail.com>
2021-06-07 10:38:14 +0430

    use some command for better code reading

--- src/App/Items/Posts/Posts.js
+++ src/App/Items/Posts/Posts.js
@@ -1,5 +1,8 @@
+// import react pkg for react project 
 import React, { Component } from 'react';
+//import axios pkg for api call
 import axios from 'axios';
+//import material ul pkg (lab/pagination0) for pagination posts 
 import Pagination from '@material-ui/lab/Pagination';
 
 
@@ -11,110 +14,83 @@ class Posts extends Component {
             Categories: [],
             Posts: [],
             Totalpages: '',
-            current_page: '',
-            post_per_page: '5',
+            CurrentPage: '',
+            PostPerPage: '5',
             page: '1',
             input_Categorie: ''
         }
     }
     componentDidMount() {
 
-        axios.get(`http://digibazi-services.naringames.com:90/v1/categories?per_page=500`)
-            .then(res => {
-                const Categories = res.data.data;
-                this.setState({ Categories });
-            }
-            );
-        axios.get(`http://digibazi-services.naringames.com:90/v1/posts?per_page=${this.state.post_per_page}&page=${this.state.page}`)
-            .then(res => {
-                const Posts = res.data.data;
-                const meta = res.data.meta;
-                const Totalpages = meta.pagination.total_pages;
-                const current_page = meta.pagination.current_page;
-                this.setState({ Posts, Totalpages, current_page });
+        //axios.get(`http://digibazi-services.naringames.com:90/v1/categories?per_page=500`)
+        //    .then(res => {
+        //        const Categories = res.data.data;
+        //        this.setState({ Categories });
+        //    }
+        //);
 
 
-            }
-            )
-
-
-    }
-    HandelSubmit = (event) => {
-        event.preventDefault()
-
-    }
-    HandelClick = () => {
-
-        axios.get(`http://digibazi-services.naringames.com:90/v1/posts?per_page=${this.state.post_per_page}&page=${this.state.page}`)
+        //call post api for give posts as digibazi api 
+        axios.get(`http://digibazi-services.naringames.com:90/v1/posts?per_page=${this.state.PostPerPage}&page=${this.state.page}`)
             .then(res => {
                 const Posts = res.data.data;
                 const meta = res.data.meta;
                 const Totalpages = meta.pagination.total_pages;
-                const current_page = meta.pagination.current_page;
-                this.setState({ Posts, Totalpages, current_page });
-
-
+                const CurrentPage = meta.pagination.CurrentPage;
+                this.setState({ Posts, Totalpages, CurrentPage });
             }
-            )
+        )
+
 
     }
+    //HandelSubmit = (event) => {
+    //    event.preventDefault()
+    //}
+    //HandelClick = () => {
+    //    this.componentDidMount();
+    //}
+
+
+    //set user input to the PostPerPage in the state
     HandelInputChange = (event) => {
         this.setState({
-            post_per_page: event.target.value,
-
+            PostPerPage: event.target.value,
         })
-
-    }
-    HandelInputChange1 = (event) => {
-        this.setState({
-            page: event.target.value,
-
-        })
-        //console.log("page = " + this.state.page)
-    }
-    HandelInputChange2 = (event) => {
-        this.setState({
-            input_Categorie: event.target.value,
-
-        })
-
     }
 
+
+    //HandelInputChange1 = (event) => {
+    //    this.setState({
+    //        page: event.target.value,
+    //    })
+    //}
+    //HandelInputChange2 = (event) => {
+    //    this.setState({
+    //        input_Categorie: event.target.value,
+    //    })
+    //}
+
+
+    //set the pagination value to the page in the state and call api 
     handlePageChange = (event) => {
-        console.log("page :" + event.target.innerText);
         this.setState({
             page: event.target.innerText,
         });
-        axios.get(`http://digibazi-services.naringames.com:90/v1/posts?per_page=${this.state.post_per_page}&page=${this.state.page}`)
-            .then(res => {
-                const Posts = res.data.data;
-                const meta = res.data.meta;
-                const Totalpages = meta.pagination.total_pages;
-                const current_page = meta.pagination.current_page;
-                this.setState({ Posts, Totalpages, current_page });
-
-
-            }
-            )
-
+        this.componentDidMount()
     }
-
-
-
-
+    
     render() {
-
         return (
             <React.Fragment>
-                <form onSubmit={this.HandelSubmit}>
+                {/* give input form user  */}
+                <form /*onSubmit={this.HandelSubmit} */ >
                     <li>
                         <label htmlFor="">post per page :</label>
-
-                        <input type="text" value={this.state.post_per_page} onChange={this.HandelInputChange} />
+                        <input type="text" value={this.state.PostPerPage} onChange={this.HandelInputChange} />
                     </li>
+                {/*
                     <li>
                         <label htmlFor="">page :</label>
-
                         <input type="text" value={this.state.page} onChange={this.HandelInputChange1} />
                     </li>
                     <li>
@@ -125,42 +101,26 @@ class Posts extends Component {
                     <li>
                         <button type='submit' onClick={this.HandelClick} >send</button>
                     </li>
-                    {/*
-                    <p>{this.state.post_per_page}</p>
-                    <p>{this.state.page}</p>
-                    <p>{this.state.input_Categorie}</p>
-                    */}
+                */}
                 </form>
-
-
-
-
-
-
+                
+                {/* pagination by material ui pkg  */}
                 <Pagination
                     variant="outlined"
-
                     onChange={this.handlePageChange}
                     count={this.state.Totalpages}
-
-                    siblingCount={0}
-                    boundaryCount={10}
                     page={this.state.page}
                 />
 
-
-
-
-
-
+                {/* show posts */}
                 <ul>
-                    {this.state.Posts.map(Post => <li key={Post.id}>
-                        <li>{Post.title}</li>
-                        <li>{Post.short_content}</li>
-
-                        <li><img src={Post.media[0].url} alt="post pic" /></li>
-                    </li>)}
-
+                    {this.state.Posts.map(Post =>
+                        <ul key={Post.id}>
+                            <li>{Post.title}</li>
+                            <li>{Post.short_content}</li>
+                            <li><img src={Post.media[0].url} alt="post pic" /></li>
+                        </ul>
+                    )}
                 </ul>
 
             </React.Fragment>
